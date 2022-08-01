0b3f26f33a60e82e05f39cddfe2327bd6f7932c4
Christian Kesler <christian.j.kesler@gmail.com>
2022-08-01 16:21:15 -0700

    Begin integrating Schema validation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import xml.etree.ElementTree as ET
 import shutil
 import glob
+import lxml
+from lxml import etree
 
 for file in glob.glob("*.xml"):
 
@@ -12,23 +14,35 @@ for file in glob.glob("*.xml"):
 	# We assume it is valid until we find an error
 	valid_xml = True
 
+	xml_file = lxml.etree.parse(file)
+	xml_validator = lxml.etree.XMLSchema(file="schema.xsd")
+
+	is_valid = xml_validator.validate(xml_file)
+
+	print(is_valid)
+
 	for book in root.findall('entry'):
-		if( len(book.find("sender").text) > 50 or len(book.find("sender").text) < 10 ):
-			valid_xml = False
+		if( isinstance(book.find("sender").text, str) ):
+			if( len(book.find("sender").text) > 50 or len(book.find("sender").text) < 10 ):
+				valid_xml = False
 
-		if( len(book.find("recipient").text) > 50 or len(book.find("recipient").text) < 10 ):
-			valid_xml = False
+		if( isinstance(book.find("recipient").text, str) ):
+			if( len(book.find("recipient").text) > 50 or len(book.find("recipient").text) < 10 ):
+				valid_xml = False
 
-		if( len(book.find("distance").text) > 8 or len(book.find("distance").text) < 3 ):
-			valid_xml = False
+		if( isinstance(book.find("distance").text, str) ):
+			if( len(book.find("distance").text) > 8 or len(book.find("distance").text) < 3 ):
+				valid_xml = False
 
-		if( len(book.find("time").text) != 4 ):
-			valid_xml = False
+		# print( book.find("time") )
+		if( isinstance(book.find("time").text, str) ):
+			if( len(book.find("time").text) != 4 ):
+				valid_xml = False
 
-	if valid_xml == True:
-		shutil.move( file, "passed/" + file )
-	elif valid_xml == False:
-		shutil.move( file, "failed/" + file )
-	else:
-		shutil.move( file, "error/" + file )
+	# if valid_xml == True:
+	# 	shutil.move( file, "passed/" + file )
+	# elif valid_xml == False:
+	# 	shutil.move( file, "failed/" + file )
+	# else:
+	# 	shutil.move( file, "error/" + file )
 
