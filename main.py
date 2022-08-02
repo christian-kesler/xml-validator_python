24f6ae1b4404eaeb38d56478aa05c674ad7d9923
Christian Kesler <christian.j.kesler@gmail.com>
2022-08-02 12:27:10 -0700

    Create sample schema validator

--- main.py
+++ main.py
@@ -1,48 +1,52 @@
 import xml.etree.ElementTree as ET
 import shutil
 import glob
-# import lxml
-# from lxml import etree
+import xmlschema
 
+# loading the xsd schema
+xsd = xmlschema.XMLSchema('shiporder.xsd')
+
+# iterating over all xml files
 for file in glob.glob("*.xml"):
 
+	# parsing file
 	tree = ET.parse(file)
 
-	root = tree.getroot()
+	# validating file against schema
+	if xsd.is_valid(tree):
 
-	# Simple boolean var for determining pass/fail status of file
-	# We assume it is valid until we find an error
-	valid_xml = True
+		# finding the root of the xml data
+		root = tree.getroot()
 
-	# This portion of code is meant to validate against a schema, but is still under development
-		# xml_file = lxml.etree.parse(file)
-		# xml_validator = lxml.etree.XMLSchema(file="schema.xsd")
+		# valid parameters boolean, we assume validity until we find an error
+		valid_params = True
 
-		# is_valid = xml_validator.validate(xml_file)
+		# iterating over all elements labeled 'entry' and comparing value fields
+		for book in root.findall('entry'):
+			if( isinstance(book.find("sender").text, str) ):
+				if( len(book.find("sender").text) > 50 or len(book.find("sender").text) < 10 ):
+					valid_params = False
 
-		# print(is_valid)
+			if( isinstance(book.find("recipient").text, str) ):
+				if( len(book.find("recipient").text) > 50 or len(book.find("recipient").text) < 10 ):
+					valid_params = False
 
-	for book in root.findall('entry'):
-		if( isinstance(book.find("sender").text, str) ):
-			if( len(book.find("sender").text) > 50 or len(book.find("sender").text) < 10 ):
-				valid_xml = False
+			if( isinstance(book.find("message").text, str) ):
+				if( len(book.find("message").text) > 8 or len(book.find("message").text) < 3 ):
+					valid_params = False
 
-		if( isinstance(book.find("recipient").text, str) ):
-			if( len(book.find("recipient").text) > 50 or len(book.find("recipient").text) < 10 ):
-				valid_xml = False
+			if( isinstance(book.find("time").text, str) ):
+				if( len(book.find("time").text) != 4 ):
+					valid_params = False
 
-		if( isinstance(book.find("message").text, str) ):
-			if( len(book.find("message").text) > 8 or len(book.find("message").text) < 3 ):
-				valid_xml = False
+		# after iteration of elements, parameter validity is checked to determine which directory the file belongs in
+		if valid_params == True:
+			shutil.move( file, "valid/" + file )
+		elif valid_params == False:
+			shutil.move( file, "invalid-params/" + file )
+		else:
+			shutil.move( file, "error/" + file )
 
-		# print( book.find("time") )
-		if( isinstance(book.find("time").text, str) ):
-			if( len(book.find("time").text) != 4 ):
-				valid_xml = False
-
-	if valid_xml == True:
-		shutil.move( file, "passed/" + file )
-	elif valid_xml == False:
-		shutil.move( file, "failed/" + file )
+	# if schema validation fails, move to invalid-schema directory
 	else:
-		shutil.move( file, "error/" + file )
+		shutil.move( file, "invalid-schema/" + file )
